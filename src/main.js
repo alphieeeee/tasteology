a8d5d646f598659975e73ce11a0861466ed6fd3d
Alps <alpsgega@gmail.com>
2025-10-18 06:23:53 +0800

    Added cards

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import './assets/fonts.css'
 import './style.css'
 import './assets/gallery.css'
 import './assets/modal.css'
-import leftImg from './images/left@2x.jpg'
-import rightTopImg from './images/right-top@2x.jpg'
-import rightBottomImg from './images/right-bottom@2x.jpg'
-import leftImgHRes from './images/left@3x.jpg'
-import rightTopImgHRes from './images/right-top@3x.jpg'
-import rightBottomImgHRes from './images/right-bottom@3x.jpg'
+import './assets/cards.css'
+import leftImg from './images/gallery/left@2x.jpg'
+import rightTopImg from './images/gallery/right-top@2x.jpg'
+import rightBottomImg from './images/gallery/right-bottom@2x.jpg'
+import leftImgHRes from './images/gallery/left@3x.jpg'
+import rightTopImgHRes from './images/gallery/right-top@3x.jpg'
+import rightBottomImgHRes from './images/gallery/right-bottom@3x.jpg'
+import redCard from './images/cards/red@2x.jpg'
+import greenCard from './images/cards/green@2x.jpg'
+import whiteCard from './images/cards/white@2x.jpg'
 import { openModal, closeModal, setModalContent } from './modal'
-
+import { createCardBatch } from './cards'
 
 document.querySelector('#app').innerHTML = `
   <main>
@@ -37,6 +41,16 @@ document.querySelector('#app').innerHTML = `
         </div>
       </div>
     </section>
+    <section id="cardsSection" class="cards-section section">
+      <div class="gutter">
+        <div class="section-title">
+          <h1 class="relative mx-auto">Taste the Colour</h1>
+        </div>
+        <div class="cards-holder">
+
+        </div>
+      </div>
+    </section>
     <div id="modalBackdrop" class="modal-backdrop fixed inset-0" aria-hidden="true">
       <div class="modal-content main-gutter" role="dialog" tabindex="-1">
         <div class="modal-header">
@@ -49,13 +63,16 @@ document.querySelector('#app').innerHTML = `
     </div>
   </main>
 `
-
+// GALLERY
 const galImages = document.querySelectorAll('.gallery-item')
 const modalBackdrop = document.getElementById('modalBackdrop');
 const modalImage = modalBackdrop.querySelector('.modal-image img');
 const modalCloseBtn = modalBackdrop.querySelector('.modal-close-btn');
 const modalContent = modalBackdrop.querySelector('.modal-content');
+// CARDS SECTION
+const cardsHolder = document.querySelector('.cards-holder');
 
+// SAMPLE OBJECTS FOR DATA
 const modalData = {
   leftImgHRes: {
     src: leftImgHRes,
@@ -74,7 +91,45 @@ const modalData = {
   }
 };
 
-// Add event listeners to gallery images on modal
+const cardsData = {
+  redCard: {
+    id: 1,
+    title: 'Red',
+    desc: 'Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.',
+    src: redCard,
+    alt: 'Red Card Image',
+    link: '#',
+  },
+  greenCard: {
+    id: 2,
+    title: 'Green',
+    desc: 'Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours',
+    src: greenCard,
+    alt: 'Green Card Image',
+    link: '#',
+  },
+  whiteCard: {
+    id: 3,
+    title: 'White',
+    desc: 'White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat.',
+    src: whiteCard,
+    alt: 'White Card Image',
+    link: '#',
+  },
+  // whiteCards: {
+  //   id: 4,
+  //   title: 'Whites',
+  //   desc: 'White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treats.',
+  //   src: whiteCard,
+  //   alt: 'White Card Images',
+  //   link: '#',
+  // },
+}
+
+// CREATE CARDS
+createCardBatch(cardsHolder, cardsData);
+
+// ADD EVENT LISTENERS
 galImages.forEach((img) => {
   img.addEventListener('click', (e) => {
     const dataObj = img.getAttribute('data-obj');
